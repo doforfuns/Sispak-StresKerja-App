43fbe701be829adc7658ef516d91ffbb50eee5f5
doforfuns <saefulsultan@gmail.com>
2023-08-10 23:53:35 +0700

    add spinner on diagnosa

--- app/src/main/java/com/sistempakarstreskerja/DiagnosaCfActivity.java
+++ app/src/main/java/com/sistempakarstreskerja/DiagnosaCfActivity.java
@@ -3,27 +3,29 @@ package com.sistempakarstreskerja;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.toolbox.JsonObjectRequest;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.HashMap;
+import com.google.android.material.textfield.MaterialAutoCompleteTextView;
 
 public class DiagnosaCfActivity extends AppCompatActivity {
 
@@ -32,8 +34,7 @@ public class DiagnosaCfActivity extends AppCompatActivity {
     private ArrayList<HashMap<String, String>> list;
     private ArrayList<Double> hasil;
     private TextView text_pertanyaan;
-    private RadioGroup rg;
-    private RadioButton rb;
+    private MaterialAutoCompleteTextView spinnerGejala;
     private int counter;
     private final int REQUEST_CODE = 1234;
 
@@ -49,21 +50,40 @@ public class DiagnosaCfActivity extends AppCompatActivity {
         getData();
 
         text_pertanyaan = findViewById(R.id.text_pertanyaan);
-        rg = findViewById(R.id.rg);
+        spinnerGejala = findViewById(R.id.spinner_gejala);
+
+        // Set up the adapter for the AutoCompleteTextView
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.gejala_options, R.layout.dropdown_item_diagnosa);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGejala.setAdapter(adapter);
+
+        spinnerGejala.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (view != null) {
+                    TextView textOption = view.findViewById(R.id.text_option);
+                    String selectedValue = parent.getItemAtPosition(position).toString();
+                    textOption.setText(selectedValue);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+
+        // Set default selection to "Pilih"
+        spinnerGejala.setText("Pilih Jawaban", false);
 
         Button btn_lanjut = findViewById(R.id.btn_lanjut);
 
         btn_lanjut.setOnClickListener(view -> {
-            int selected = rg.getCheckedRadioButtonId();
-            if (selected == -1) {
-                Toast.makeText(this, "Pilih dulu salah satu jawaban", Toast.LENGTH_SHORT).show();
-            } else {
-                rb = findViewById(selected);
-                hasil.add(konversiJawaban(rb.getText().toString()));
-                showPertanyaan(++counter);
-            }
+            String selectedValue = spinnerGejala.getText().toString();
+            hasil.add(konversiJawaban(selectedValue));
+            showPertanyaan(++counter);
         });
-
     }
 
     private double konversiJawaban(String value) {
@@ -91,15 +111,23 @@ public class DiagnosaCfActivity extends AppCompatActivity {
         }
     }
 
+    private void setSpinnerToDefault() {
+        spinnerGejala.setSelection(0); // 0 corresponds to the index of "Pilih"
+    }
+
     private void showPertanyaan(int index) {
         if (index >= list.size()) {
             Intent intent = new Intent(getApplicationContext(), DiagnosaCfHasilActivity.class);
             intent.putExtra("hasil", android.text.TextUtils.join("#", hasil));
             startActivityForResult(intent, REQUEST_CODE);
+            // Set Spinner back to default
+            setSpinnerToDefault();
         } else {
             HashMap<String, String> item = list.get(index);
             text_pertanyaan.setText("Apakah " + item.get("nama_gejala").toLowerCase() + "?");
-            rg.clearCheck();
+            // Set Spinner back to default
+            setSpinnerToDefault();
+            spinnerGejala.setText("Pilih Jawaban", false); // Set default text for the spinner
         }
     }
 
